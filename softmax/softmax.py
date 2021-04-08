ffe99e59ac2d40c17b1e7f6e7591d0a2db16d950
sayhoChun <fishcreek@naver.com>
2021-04-09 02:43:41 +0900

    softmax added

--- softmax/softmax.py
+++ softmax/softmax.py
@@ -26,12 +26,6 @@ mnist_train = dsets.MNIST(root='MNIST_data/',
                           train=True,
                           transform=transforms.ToTensor(),
                           download=True)
-
-mnist_test = dsets.MNIST(root='MNIST_data/',
-                         train=False,
-                         transform=transforms.ToTensor(),
-                         download=True)
-
 # dataset loader
 data_loader = DataLoader(dataset=mnist_train,
                          batch_size=batch_size,  # 배치 크기는 100
@@ -79,26 +73,3 @@ print('Learning finished')
 # for var_name in optimizer.state_dict():
 #     print(var_name, "\t", optimizer.state_dict()[var_name])
 torch.save(model.state_dict(), "softmax/out/softmaxModel")
-
-
-# 테스트 데이터를 사용하여 모델을 테스트한다.
-# with torch.no_grad():  # torch.no_grad()를 하면 gradient 계산을 수행하지 않는다.
-#     X_test = mnist_test.test_data.view(-1, 28 * 28).float().to(device)
-#     Y_test = mnist_test.test_labels.to(device)
-#
-#     prediction = model.forward(X_test)
-#     correct_prediction = torch.argmax(prediction, 1) == Y_test
-#     accuracy = correct_prediction.float().mean()
-#     print('Accuracy:', accuracy.item())
-#
-#     # MNIST 테스트 데이터에서 무작위로 하나를 뽑아서 예측을 해본다
-#     r = random.randint(0, len(mnist_test) - 1)
-#     X_single_data = mnist_test.test_data[r:r + 1].view(-1, 28 * 28).float().to(device)
-#     Y_single_data = mnist_test.test_labels[r:r + 1].to(device)
-#
-#     print('Label: ', Y_single_data.item())
-#     single_prediction = model.forward(X_single_data)
-#     print('Prediction: ', torch.argmax(single_prediction, 1).item())
-#
-#     plt.imshow(mnist_test.test_data[r:r + 1].view(28, 28), cmap='Greys', interpolation='nearest')
-#     plt.show()
